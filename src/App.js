90c0ca33d926867ced9f9ea37ebe20920baee257
oscar02r <oscary.ramirez@gmail.com>
2020-05-27 17:50:09 +0200

    Adding component editProduct

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './components/Header';
 import Productos from './components/Productos';
+import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
 
 
 function App() {
   return (
     <Router>
       <Header/>
+      <div className="container mt-5">
       <Switch>
         <Route exact path="/" component={Productos}/> 
-        <Route exact path="/productos/nuevo" />
+        <Route exact path="/productos/nuevo" component={NuevoProducto} />
+        <Route exact path="/productos/editar/:id" component={EditarProducto}/>
       </Switch>
+      </div>
     </Router>
   );
 }
