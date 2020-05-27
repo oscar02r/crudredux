1507997410b79b4249da24c1f7864b4dd4220dbb
oscar02r <oscary.ramirez@gmail.com>
2020-05-27 20:58:22 +0200

    Config store

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import Header from "./components/Header";
 import Productos from "./components/Productos";
 import NuevoProducto from "./components/NuevoProducto";
 import EditarProducto from "./components/EditarProducto";
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
     <Router>
+
+      <Provider store = {store}>
       <Header />
       <div className="container mt-5">
         <Switch>
@@ -17,6 +21,7 @@ function App() {
           />
         </Switch>
       </div>
+      </Provider>
     </Router>
   );
 }
